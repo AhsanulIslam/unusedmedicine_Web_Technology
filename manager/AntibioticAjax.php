706f25613eaedab1b4728c571473ae23f70184f1
Ahsanulislam <imon.ahsanul.islam@gmail.com>
2020-11-26 17:10:00 +0600

    manager ajax

---
+++ manager/AntibioticAjax.php
@@ -0,0 +1,80 @@
+<?php
+include "../connection/dbConnection.php";
+
+ if(isset($_POST['searchval'])){
+        $sv = $_POST['searchval'];
+         if(!empty($_POST['searchval'])){
+        $Query = "select * from medicine where category='Antibiotic' and med_name like '$sv%' or ExpDate like '$sv%' or batchid like  '$sv%' or brand like  '$sv%'   limit 5";
+         }
+         else
+         {
+         $Query = "select * from medicine where category='Antibiotic' limit 5";
+         }
+          $result = mysqli_query($conn, $Query);
+    }
+ else if(isset($_POST['searchval1'])){
+        $sv = $_POST['searchval1'];
+        $Query = "select * from medicine where category='Antibiotic' and med_name like '$sv%' limit 5";
+        $result = mysqli_query($conn, $Query);
+ }
+else if(isset($_POST['searchval2'])){
+        $sv = $_POST['searchval2'];
+        $Query = "select * from medicine where category='Antibiotic' and brand like '$sv%' limit 5";
+        $result = mysqli_query($conn, $Query);
+ }
+ else{
+     $Query = "select * from medicine where category='Antibiotic' limit 5";
+     $result = mysqli_query($conn, $Query);
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="displayData" id="medicine">
+              <table>
+                    <tr>
+                        
+                        <th>Medicine Name</th>
+                        <th>Expired Date</th>
+                        <th>Batch id</th>
+                        <th>Brand</th>
+                        <th>Remaining Days To expired</th>
+                       
+                    </tr>
+                    <tbody>
+                    <?php while($row = $result->fetch_assoc()):  ?>
+                       
+                            <tr>
+                           
+                            <td> <?php echo $row["med_name"] ;?> </td>
+                            <td> <?php echo $row["ExpDate"] ;?> </td>
+                            <td> <?php echo $row["batchid"]; ?> </td>
+                            <td> <?php echo $row["brand"] ;?></td>
+                          
+                           <td> 
+                            <?php $now =strtotime( date("Y-m-d"));
+                           $expd =  strtotime( $row["ExpDate"] ); 
+                            $value= ceil(abs($expd -$now)/86400);
+                           if($value>0){
+                             echo $value;
+                           }
+                           else{
+                             echo $value =0;
+                           }
+                           ?>
+                           </td>
+
+                            </tr> 
+                      <?php endwhile; ?>
+                       </tbody>       
+            </table>
+        </div>
+</body>
+</html>
---
+++ manager/crudfilterAjax.php
@@ -0,0 +1,71 @@
+<?php
+include "../connection/dbConnection.php";
+
+ if(isset($_POST['searchval'])){
+        $sv = $_POST['searchval'];
+         if(!empty($_POST['searchval'])){
+        $Query = "select * from medicine where med_name like '$sv%' or ExpDate like '$sv%' or category like '$sv%'  or batchid like  '$sv%' or brand like  '$sv%'   limit 5";
+         }
+         else
+         {
+         $Query = "select * from medicine limit 5";
+         }
+          $result = mysqli_query($conn, $Query);
+    }
+ else if(isset($_POST['searchval1'])){
+        $sv = $_POST['searchval1'];
+        $Query = "select * from medicine where med_name like '$sv%' limit 5";
+        $result = mysqli_query($conn, $Query);
+ }
+else if(isset($_POST['searchval2'])){
+        $sv = $_POST['searchval2'];
+        $Query = "select * from medicine where category like '$sv%' limit 5";
+        $result = mysqli_query($conn, $Query);
+ }
+ else{
+     $Query =  "select * from medicine";
+     $result = mysqli_query($conn, $Query);
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+ <div class="displayData" id="medicine">
+              <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Medicine Name</th>
+                        <th>Expired Date</th>
+                        <th>Category</th>
+                        <th>Batch id</th>
+                        <th>Brand</th>
+                        <th>Action</th>
+                    </tr>
+                    <tbody>
+                    <?php while($row = $result->fetch_assoc()):  ?>
+                       
+                            <tr>
+                            <td> <?php echo  $row["med_id"] ; ?> </td>
+                            <td> <?php echo $row["med_name"] ;?> </td>
+                            <td> <?php echo $row["ExpDate"] ;?> </td>
+                            <td> <?php echo $row["category"] ;?> </td>
+                            <td> <?php echo $row["batchid"]; ?> </td>
+                            <td> <?php echo $row["brand"] ;?></td>
+                            <td>  
+                          <a href="UpdateMedicine.php?edit= <?php echo $row['med_id']; ?>" class="edit_btn">Edit</a> 
+                               
+                            </td>
+                            </tr> 
+                      <?php endwhile; ?>
+                       </tbody>       
+            </table>
+        </div>
+</body>
+</html>
